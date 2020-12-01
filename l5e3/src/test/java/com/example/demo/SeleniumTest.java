0aa712d965717f8d0d9056706b8b371bb18a772f
Ana Sustic <ana.sustic@uzh.ch>
2020-12-01 09:56:35 +0100

    Added changes to the SeleniumTest

--- l5e3/src/test/java/com/example/demo/SeleniumTest.java
+++ l5e3/src/test/java/com/example/demo/SeleniumTest.java
@@ -1,35 +1,50 @@
 package com.example.demo;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SeleniumTest {
+
+
+    @LocalServerPort
+    private static Integer port;
+
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void beforeAll(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        driver.close();
+    }
+
     @Test
     public static void main(String[] args) throws InterruptedException {
         //start the driver, open chrome to our target url
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:8080/animal");
+        driver.get("http://localhost:"+port+"/animal");
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
         //find the fields we want by id and fill them in
         WebElement inputField = driver.findElement(By.id("animalText"));
         inputField.sendKeys("Manatee");
-
         inputField = driver.findElement(By.id("adjective"));
         inputField.sendKeys("Whirling");
-
         List<WebElement> trainingResults = driver.findElements(By.className("trainingMessage"));
-
         // The field-values don’t clear on submit for our simple app, so just submit it 5 times
         // However, the elements gets removed from the DOM structure after each submit.
         for(int i = 0; i < 5; i++) {
@@ -37,15 +52,11 @@ public class SeleniumTest {
             // Otherwise, you'll get org.openqa.selenium.StaleElementReferenceException at runtime.
             inputField = driver.findElement(By.id("adjective"));
             inputField.submit();
-
             System.out.println("trainingResults.size() = " + trainingResults.size());
         }
-
         // then get the element by the class conclusionMessage and print it
         WebElement conclusionResult = driver.findElement(By.className("conclusionMessage"));
         System.out.println("conclusionResult.getText() = " + conclusionResult.getText());
-
         Thread.sleep(5000);
-        driver.quit();
     }
 }
